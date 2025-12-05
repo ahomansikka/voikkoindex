70d8cec9814449224cbede37e27b2320488e40ef
Hannu Väisänen <Hannu.Vaisanen@uef.fi>
2025-12-05 10:11:03 +0200

    Bugin korjaus. Komennossa \VXN{Matti}{\emph{Virtaselle:}} hyväksytään myös kaksoispiste näiden lisäksi .,;

--- voikkoindex.lua
+++ voikkoindex.lua
@@ -175,7 +175,7 @@ local function cleanup (s)
   t = utf8.gsub (t, "\"", "")               -- Poistetaan tavutusohje esim. linja"-autosta => linja-autosta.
   t = utf8.gsub (t, "\n", " ")              -- Muutetaan rivinvaihto tyhjeeksi.
   t = utf8.gsub (t, "\\emph%s*{", "")       -- Esim. "\emph{Virtanen}" => "Virtanen}" tai "\emph{Virtanen.}" => "Virtanen.}"
-  t = utf8.gsub (t, "[.,;]}", "")           -- Poistetaan \emph{}:n lopusta jäljelle jääneet merkkijonot ".}", ",}" tai ";}".
+  t = utf8.gsub (t, "[.,;:]}", "")          -- Poistetaan \emph{}:n lopusta jäljelle jääneet merkkijonot ".}", ",}" tai ";}".
   t = utf8.gsub (t, "[{}]", "")             -- Poistetaan mahdollisesti jäljelle jääneet { ja }.
   return t
 end
